fbb206f955e6899b3d56fb1fa28f05fc2221719c
Jeetendra Kumar <67962129+jeetendra-jeet@users.noreply.github.com>
2025-03-11 02:19:35 +0530

    SortArray.js

--- SortArray.js
+++ SortArray.js
@@ -1,1 +1,13 @@
+let arr = [5,3,7,1,9,3,66,23,12,99,34];
 
+for (var i = 0; i < arr.length; i++) {
+  for (var j = 0; j < arr.length; j++) {
+    if(arr[j] > arr[j+1]){
+      let temp = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+    }
+  }
+}
+
+console.log(arr);
