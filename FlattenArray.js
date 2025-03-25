dba77604ce64f7be4a1381cf48e58d77cc90df71
Jeetendra Kumar <67962129+jeetendra-jeet@users.noreply.github.com>
2025-03-26 02:50:27 +0530

    Updateed FlattenArray

--- FlattenArray.js
+++ FlattenArray.js
@@ -1,13 +1,15 @@
-let arr = [5,3,[7,1,9],3,[66,23],[12,99,34]];
-let newArr = []
-function flatArray(arr){
-for (var i = 0; i < arr.length; i++) {
-    if(arr[i].length !== undefined){
-      flatArray(arr[i])
-    }else{
-      newArr.push(arr[i])
+let arr = [4,3,[7,2,4],5,[3,6],4,[5,7]]
+let newArr = [];
+function flatArr(arr){
+    for(let i = 0; i< arr.length; i++){
+        // if(Array.isArray(arr[i])){ // using inbuilt method
+        if(arr[i].length !== undefined){ // without inbuilt method
+            flatArr(arr[i])
+        }else{
+            newArr.push(arr[i])
+        }
     }
+    return newArr
 }
-}
-flatArray(arr)
-console.log(newArr);
+console.log('Original', arr)
+console.log('FlatenArr',flatArr(arr))
